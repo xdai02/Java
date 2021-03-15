6a720d337660ccf07f22beb62bcfd9f576aab9d9
xdai02 <2513276112@qq.com>
2021-03-15 23:17:52 +0800

    add chapter 1 - 2

---
+++ 第2章 数据类型/2.3 初始化/Initialization.java
@@ -0,0 +1,8 @@
+public class Initialization {
+	public static void main(String[] args) {
+		int n = 10;
+		double wage = 8232.56;
+		System.out.println(n);
+		System.out.println(wage);
+	}
+}
---
+++ 第2章 数据类型/2.5 输入输出函数/CircleArea.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CircleArea {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		final double PI = 3.14159;
+		double r;
+		double area;
+		
+		System.out.print("ÊäÈë°ë¾¶£º");
+		r = scanner.nextDouble();
+		area = PI * r * r;
+		
+		System.out.println(String.format("Ãæ»ý£º%.2f", area));
+		scanner.close();
+	}
+}
