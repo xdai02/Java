655835dc26bd70f5434266a1d884cf9ba042ad87
xdai02 <2513276112@qq.com>
2022-01-20 20:58:01 +0800

    reformat chapter 1 + 2

--- code/第2章 数据类型/2.5 类型转换/Explicit.java
+++ code/第2章 数据类型/2.5 类型转换/Explicit.java
@@ -2,9 +2,7 @@ public class Explicit {
 	public static void main(String[] args) {
 		int sum = 821;
 	    int num = 10;
-	    double average;
-
-	    average = (double)sum / num;
+	    double average = (double)sum / num;
 	    System.out.println(String.format("average = %.2f", average));
 	}
 }
