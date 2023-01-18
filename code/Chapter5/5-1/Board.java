2d45e2a89c544f2ca75d96d8698fd2bbf9fd763e
xdai02 <2513276112@qq.com>
2023-01-18 23:24:49 +0800

    fix

--- code/Chapter5/5-1/Board.java
+++ code/Chapter5/5-1/Board.java
@@ -1,16 +1,16 @@
 public class Board {
     public static void main(String[] args) {
-        print_board(3, 3);
+        printBoard();
     }
 
-    public static void print_board(int row, int col) {
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col - 1; j++) {
+    public static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
                 System.out.print("   |");
             }
             System.out.println();
 
-            if (i < row - 1) {
+            if (i < 2) {
                 System.out.println("---+---+---");
             }
         }
